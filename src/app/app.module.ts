6bf2f1173debb8df55556c6f9655d8e9f1b5e45a
nabatwadenis <dnabatwa9@gmail.com>
2023-02-03 17:30:13 +0300

    basic angular bootstrap blog website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
+import { IntroComponentComponent } from './intro-component/intro-component.component';
+import { BlogComponentComponent } from './blog-component/blog-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponentComponent,
+    IntroComponentComponent,
+    BlogComponentComponent,
+    FooterComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/intro-component/intro-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-component',
+  templateUrl: './intro-component.component.html',
+  styleUrls: ['./intro-component.component.css']
+})
+export class IntroComponentComponent {
+
+}
